b8239e323c65f0c14b31301ec0abf962e7e043df
Grzegorz Lippe <grzegorz.lippe@googlemail.com>
2022-08-23 13:56:37 +0200

    Able to crop

---
+++ create_hoerbert_mix.py
@@ -0,0 +1,44 @@
+import json
+import youtube_dl
+#import matplotlib.pyplot as plt
+
+from pathlib import Path
+#from slugify import slugify
+
+with open('hoerbert.json', 'r', encoding='utf-8') as f:
+    hoerbert = json.load(f)
+
+destination = Path('sd_card')
+destination.mkdir(exist_ok=True, parents=True)
+
+for idb, button in enumerate(hoerbert):
+    for ids, song in enumerate(hoerbert[button]):
+        if idb != 2 or ids != 2:
+            continue 
+        song_name = '%d.%%(ext)s' % (ids)
+        song_path = destination / str(idb) / song_name
+        
+        print('\n%s\n%s\n' % (song['Name'], '=' * len(song['Name'])))
+        
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': str(song_path),
+            'postprocessors': [
+                {
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '128',
+                }
+            ],
+            'postprocessor_args' : []
+            }
+
+        if len(song['crop_start']) > 0:
+            ydl_opts['postprocessor_args'].extend(['-ss', song['crop_start']])
+
+        if len(song['crop_tail']) > 0:
+            ydl_opts['postprocessor_args'].extend(['-to', song['crop_tail']])
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.cache.remove()
+            ydl.download([song['Link']])
